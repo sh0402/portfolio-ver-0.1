32cd6c174ad09c3c78c219c2094aed28a8faed7f
sh0402 <dgryoma0402@gmail.com>
2021-10-29 01:11:30 +0900

    01

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,45 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+	<v-app>
+		<v-app-bar app color="white" light>
+			<v-container class="d-flex">
+				<div class="d-flex align-center">
+					<v-img
+						alt="Vuetify Logo"
+						class="shrink mr-4"
+						contain
+						src="@/assets/logo.png"
+						transition="scale-transition"
+						width="40"
+					/>
+				</div>
+
+				<v-toolbar-items>
+					<v-btn text> Home </v-btn>
+					<v-btn text> About </v-btn>
+					<v-btn text> Projects </v-btn>
+					<v-btn text> Contact </v-btn>
+				</v-toolbar-items>
+
+				<v-spacer></v-spacer>
+
+				<v-btn icon>
+					<v-icon>mdi-account</v-icon>
+				</v-btn>
+			</v-container>
+		</v-app-bar>
+
+		<v-main>
+			<router-view />
+		</v-main>
+	</v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+	name: 'App',
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+	data: () => ({
+		//
+	})
+};
+</script>
